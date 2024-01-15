7baf5f3b4afebd340fbca00ff35062d12f3390fa
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2024-01-15 13:05:48 +0100

    :building_construction: Move index.html and adjust start/build scripts.

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
         "assetsDir": "./",
         "outDir": "./build",
         "cssCodeSplit": false,
-        "rollupOptions": {"input": {"app": "./src/index.html"}},
+        "rollupOptions": {"input": {"app": "./index.html"}},
     },
     "test": {
         "globals": true,
@@ -32,5 +32,4 @@ export default defineConfig({
         "setupFiles": ["src/tests/setup.ts"],
         "env": {"IS_REACT_ACT_ENVIRONMENT": "true"},
     },
-    "server": {"open": "./src/index.html"},
 });
